6911906076fb6a44cf3e9fdd97c0fb71cf7a9022
zxb008 <1871819749@qq.com>
2019-07-04 10:26:39 +0800

    拼多多

--- buytogether/src/router/index.js
+++ buytogether/src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/Home/Home'
+import Recommend from '@/pages/Recommend/Recommend'
+import Chat from '@/pages/Chat/Chat'
+import Search from '@/pages/Search/Search'
+import Me from '@/pages/Me/Me'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      redirect:'/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/chat',
+      component: Chat
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/me',
+      component: Me
     }
   ]
 })
