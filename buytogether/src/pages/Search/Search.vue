cccd192ddb3027dabae35c948536d1cee39e4d6e
zxb008 <1871819749@qq.com>
2019-07-08 16:55:10 +0800

    拼多多

--- buytogether/src/pages/Search/Search.vue
+++ buytogether/src/pages/Search/Search.vue
@@ -2,21 +2,23 @@
   <div class="search">
     <SearchNav/>
     <div class="search-bot">
-      <div class="left">
+      <div class="left" ref="left">
         <ul>
           <!-- <li class="left-li current" v-for="(goods, index) in searchgoods" :key="index"><span>{{goods.name}}</span></li> -->
           <li class="left-li " v-for="(goods, index) in searchgoods" :key="index"><span>{{goods.name}}</span></li>
         </ul>
       </div>
-      <div class="right">
+      <div class="right" ref="right">
         <ul>
           <li class="right-li" v-for="(goods, index) in searchgoods" :key="index">
             <div class="li-title">
               <span>{{goods.name}}</span>
               <a href="">查看全部 ></a>
             </div>
-            <ul v-if="false" class="li-img">
-              <li><img src="./images/s1.png" alt=""></li>
+            <ul v-if="goods.tag === 'phone'" class="li-img">
+              <li v-for="(phone, index) in goods.category" :key="index">
+                <img :src="phone.icon" alt="">
+              </li>
             </ul>
             <ul class="li-items">
               <li class="items-li" v-for="(item, itemIndex) in goods.items" :key="itemIndex">
@@ -32,6 +34,7 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 import {mapState} from 'vuex'
 import SearchNav from './Children/SearchNav'
 export default {
@@ -50,6 +53,22 @@ export default {
    //使得vuex里面的state获取到搜索板块的商品数据
    this.$store.dispatch('reqSearchGoods');
  },
+ watch: {
+   searchgoods () {
+     this.$nextTick(() => {
+          this.initScroll();
+        })
+   }
+ },
+ methods: {
+   initScroll () {
+     let scrollLeft  = new BScroll(this.$refs.left,{
+        scrollY: true,
+        click: true
+     });
+     let scrollRight  = new BScroll(this.$refs.right,{});
+   }
+ },
 }
 </script>
 
@@ -65,13 +84,14 @@ export default {
     bottom 50px
     width 100%
     display flex
+    overflow hidden  //超出的部分隐藏
     .left
       flex 1  // flex 1 1 0
-      height 100%
+      // height 100% 不能设置高度 否则scroll不了
       // background-color blue
       ul
         width 100%
-        height 100%
+        // height 100% 不能设置高度 
         .left-li
           width 100%
           height 60px
@@ -95,11 +115,11 @@ export default {
             left 0px
     .right
       flex 4  // flex 4 1 0
-      height 100%
+      // height 100%
       background-color #fff
       >ul
         width 100%
-        height 100%
+        // height 100%
         .right-li
           width 100%
           // height 子元素会撑起来
@@ -126,13 +146,13 @@ export default {
             flex-wrap wrap
             border-bottom-1px(#ccc)
             li
-            width 33.3%
-            display flex
-            justify-content center
-            align-items center
-            margin 5px 0
-            img
-              width 70%
+              width 33.3%
+              display flex
+              justify-content center
+              align-items center
+              margin 5px 0
+              img
+                width 70%
           .li-items
             width 100%
             display flex
