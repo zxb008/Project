60a3625bdd0db25883f96b590e868acea34bb04a
zxb008 <1871819749@qq.com>
2019-07-27 14:32:51 +0800

    7-27

--- backgroundsystem/src/App.js
+++ backgroundsystem/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+import store from './Store/index'
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Header from './Components/Header/Header'
+
 
 function App() {
   return (
-    <div className="App">
-     hello word
-    </div>
+    <Router>
+      <Provider store={store}>
+        <div>
+          <Header/>
+       </div>
+      </Provider>
+    </Router>
   );
 }
 
---
+++ backgroundsystem/src/Store/index.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './reducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(reducer, enhancer);//创建数据仓库
+export default store;//暴露出去
