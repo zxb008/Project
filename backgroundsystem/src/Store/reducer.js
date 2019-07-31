d6784921aff1a50cf2ddfa7a800368ce1d6e5833
zxb008 <1871819749@qq.com>
2019-07-31 16:19:31 +0800

    7-31

--- backgroundsystem/src/Store/reducer.js
+++ backgroundsystem/src/Store/reducer.js
@@ -1,7 +1,19 @@
 const defaultState = {
-
+  "login_user": "212",
+  "new_register": "121",
+  "new_stu_course": "232",
+  "new_stu_classes": "323",
+  "new_member": "221",
+  "not_reply": "11",
+  "order_counter": {
+    "web": "122",
+    "java": "32",
+    "python": "09",
+    "bigdata": "60",
+    "ui": "99"
+  }
 }
 
-export default  (state = defaultState, action) => {
+export default (state = defaultState, action) => {
   return state
 }
